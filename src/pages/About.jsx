310d8cfb117c73b537bbcaee7ac2e137b52dbb24
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-04-07 21:23:41 +0700

    fix about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,68 +1,6 @@
 import Quotes from '../assets/quotes.svg';
 import Photo from '../assets/foto.svg';
-
-const styles = {
-    container: {
-        display: 'flex',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: '100%',
-        padding: '7%',
-        boxSizing: 'border-box',
-        justifyContent: 'space-between'
-    },
-    itemContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '100%',
-        height: '100%',
-        padding: '0 220px 0 113px',
-        boxSizing: 'border-box'
-    },
-    quotes: {
-        display: 'flex',
-        marginLeft: '150'
-    },
-    text: {
-        display: 'flex',
-        width: '462px',
-        height: '110px',
-        fontFamily: 'Quicksand',
-        fontStyle: 'normal',
-        fontWeight: '700',
-        fontSize: '40px',
-        lineHeight: '50px',
-        marginLeft: '60px'
-    },
-    card: {
-        background: '#FFFFFF',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-        transition: '0.3s',
-        borderRadius: '5px',
-        width: '529px',
-        height: '600px',
-        margin: '0 50px',
-        marginRight: '100px',
-        padding: '20px'
-    },
-    paragraph: {
-        width: '449px',
-        height: '576px',
-        margin: '24px 40px',
-        fontFamily: 'Quicksand',
-        fontStyle: 'normal',
-        fontWeight: '400',
-        fontSize: '20px',
-        lineHeight: '48px'
-    },
-    photo: {
-        width: '529px',
-        height: '575px',
-        marginTop: '175px'
-
-    }
-};
+import styles from "../pages/About.Style.js";
 
 const About = () => {
     return (
@@ -81,11 +19,11 @@ const About = () => {
                 </div>
                 <div>
                     <img src={Photo} style={styles.photo} />
-                    <span>Test</span>
-                    <span>test</span>
+                    <span style={styles.nametext}>Ir.H.Ady Setiawan.SH<br/></span>
+                    <span style={styles.departmenttext}>President of PDAM Purwa Tirta Dharma</span>
                 </div>
             </div>
-            <button>Test</button>
+            <button style={styles.button}><span style={styles.textButton}>Lebih Lengkap</span></button>
         </div>
     );
 };
